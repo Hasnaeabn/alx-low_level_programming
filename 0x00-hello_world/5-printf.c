2d53c2d7c296ef1adf2c3483aa57f813cd3e0be3
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-16 06:45:15 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main -printf
  * print specific message using printf
- * return : Always 0 (Success)
+ * main return : Always 0 (Success)
  */
 int main(void)
 {
-	printf( "with proper grammar, but the outcome is a piece of art,\n");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
