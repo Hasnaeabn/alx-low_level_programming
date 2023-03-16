3b1283c084c52b2ce25dbdd6fd2b26853f0c8459
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-16 06:25:42 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main print specific message
- * description : Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
+ * description : Programming is like building a multilingual puzzle, followed by a new line.
  * prameter puts
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void) 
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
