98a7f9cf6c65773b389cb271b01baf2dfdff87e8
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-17 03:22:55 -0700

    ase

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+
 /**
- *main Size of various type based onthe computer 
- *Return: Always (0)
+ * main -Size of various type based onthe computer
+ * Return:Always (0)
  */
 
 int main(void)
