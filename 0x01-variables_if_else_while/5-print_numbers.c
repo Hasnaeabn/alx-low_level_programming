5750ddb2167d0aab102297947bc97e0cf9376245
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-19 13:12:00 -0700

    add

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,13 +10,12 @@
 
 int main(void)
 {
-	int i = 0;
-	while (i < 10)
-	{
-		putchar(i);
-		i++;
-	}
-	putchar('\n');
+       	int i;
 
-	return(0);
+	for (i = 0;i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	return (0);
 }
