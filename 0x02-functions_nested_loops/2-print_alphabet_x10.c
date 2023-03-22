edeecde30282122e2bd8b2facb6484cafe009ed6
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-22 03:32:05 -0700

    add loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -Write a function that prints 10 times the alphabet
+ * in lowercase
+ * followed by a new line
+ * Return nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 97; 
+	int j;
+
+	while (i < 123)
+	{
+		j = 0;
+
+		while (j < 11)
+		{
+		_putchar(i);
+		j+;
+		}
+		i++;
+	}
+}
