b7d063085afc964a04ae50bcc9d7d935070de1f9
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-22 03:10:33 -0700

    add

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * main -I sometimes suffer from insomnia. And when I can't fall asleep
+ * I play what I call the alphabet game
+ * description: Write a function that prints the alphabet, in lowercase
+ * followed by a new line.
+ * Prototype: void print_alphabet(void);
+ * You can only use _putchar twice in your code
+ * Return : 0(sucess)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
