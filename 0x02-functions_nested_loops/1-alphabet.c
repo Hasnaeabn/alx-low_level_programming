71194f5f9ea7e411598368e1512369c636d678e3
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-22 03:20:44 -0700

    add

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include"main.h"
 
 /**
- * print_alphabet: -I sometimes suffer from insomnia. And when I can't fall asleep
+ * print_alphabet -I sometimes suffer from insomnia.
+ * And when I can't fall asleep
  * I play what I call the alphabet game
  * description: Write a function that prints the alphabet, in lowercase
  * followed by a new line.
