3cf9f4e976be21931094898ef98de33d977eb545
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-22 02:49:17 -0700

    umodate

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - 0. _putchar
  * description : Write a program that prints _putchar, followed by a new line.
  * The program should return 0
- * return : 0 sucess
+ * Return: 0(sucess)
  */
 
 int main(void)
