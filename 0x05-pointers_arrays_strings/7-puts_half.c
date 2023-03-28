498bb4babadf9bee2fdd5d08d9931cfa80e2b15c
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-28 07:07:40 -0700

    add

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,18 +9,17 @@
 
 void puts_half(char *str)
 {
-	int l =0;
+	int l = 0;
 	int n;
 	
-
-	for (; str[l] != '\0'; l++)
+	while (str[l] != '\0')
 	{
 		l++;
 	}
 
 	if (l % 2 != 0)
 	{
-		n = (l - 1)/2;
+		n = (l - 1) / 2;
 		n = n + 1;
 	}
 
@@ -29,9 +28,8 @@ void puts_half(char *str)
 		n = l / 2;
 	}
 	for (; n < l; n++)
-		{
-			_putchar(str[n]);
-		}
+	{
+		_putchar(str[n]);
+	}
 	_putchar('\n');
-	
 }
