aea11646c77dba18f2c2fb588293e9edf6fcceb2
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-28 07:44:48 -0700

    add

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * _strcpy - Copy a string
+  * @dest: the buffer
+  * @src: the string to copy
+  *
+  * Return: the pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int l;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		l = i++;
+	}
+
+	dest[l] = '\0';
+}
