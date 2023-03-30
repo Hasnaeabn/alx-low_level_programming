ee4e7a9e66bebb33db690bc29ffb13ff1d39cdb6
Hasnaeabn <hasna.abounouh@gmail.com>
2023-03-30 00:38:19 -0700

    add

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,20 +2,19 @@
 
 /**
  * _strcmp - similaire to strcmp
- * @s& : first string to compare
+ * @s1 : first string to compare
  * @s2 : seconde string to compare
  * Return : integer
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int n = 15;
-	if (s1 < s2)
-		n = n * (-1);
-	else if (s1 > s2)
-		n = n * 1;
-	else 
-		n = n * 0;
-	
-	return(n);
+
+	while(*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return *(unsigned char *)s1 - *(unsigned char *)s2;
+
 }
