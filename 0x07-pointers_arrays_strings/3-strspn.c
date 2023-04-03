d8449475313a6f037009af748826b82f0ffa9d5e
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-03 14:39:35 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include"main.h"
+
+/** _strspn() see description
+ * @s : initial segement
+ * @accept : bytes from accept
+ * Return : the number of bytes in the initial segment of s which consis
+ * only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int found;
+
+	for (; *s != '\0'; s++)
+	{
+		found = 0;
+	}
+
+	for (; *accept !='\0'; accept++)
+	{
+		if (*s == *accept)
+		{
+			found = 1;
+			break;
+		}
+	}
+
+	if (found = 1)
+	{
+		i++;
+	}
+	else 
+	{
+		break;
+	}
+	return(i);
+}
