c2cd7438a98b5c8d472a851e7bc4472a44c87a3d
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-04 06:38:44 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,11 @@
 #include"main.h"
 #include<stdio.h>
 
-/** pritn_chessboard() -function that prints the chessboard
+/**
+ * pritn_chessboard() -function that prints the chessboard
  * @a : the array
- * Return : void */
+ * Return : void
+ */
 
 
 void print_chessboard(char (*a)[8])
