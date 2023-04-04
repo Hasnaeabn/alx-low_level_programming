46d4a284b58a5376feeae7aa77c355ff4c7f5ea8
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-04 06:45:10 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,8 @@
 #include"main.h"
 #include<stdio.h>
 
-/** _strstr() - see description
+/**
+ * _strstr() - see description
  * @haystack: the input string
  * @needle :the string to find
  * Return : pointer to the beginning
@@ -12,29 +13,29 @@ char *_strstr(char *haystack, char *needle)
 	char *a = haystack;
 	char *b = needle;
 
-	while(*haystack != '\0')
+	while (*haystack != '\0')
 	{
 		a = haystack;
 		b = needle;
-
-		while( *a != '\0' && *b != '\0' && *a == *b)
+		
+		while ( *a != '\0' && *b != '\0' && *a == *b)
 		{
 			a++;
 			b++;
 		}
-
+		
 		if (*b == '\0')
 		{
-			return(haystack);
+			return (haystack);
 		}
 
 		haystack++;
 	}
 
 	if (*needle == '\0')
-        {
-                return(haystack);
-        }
+	{
+                return (haystack);
+	}
 
-	return(NULL);
+	return (NULL);
 }
