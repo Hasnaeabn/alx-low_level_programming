47b57d5620ea735ac0a34fd4be7f44e3c940d2e8
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-12 07:22:20 -0700

    add concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,42 +11,37 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	int i = 0;
+	int j = 0;
+	int l;
+	char *s = (char *)malloc(sizeof(char) * (i + j + 1));
+
 	if (s1 || s2 == NULL)
 	{
 		s1 = "";
 		s2 = "";
 	}
 
-	int i = 0;
-
 	while (s1[i] != '\0')
 	{
 		i++;
 	}
-
-	int j = 0;
 
 	while (s2[j] != '\0')
 	{
 		j++;
 	}
 
-	char *s = (char *)malloc( sizeof(char) * (i + j + 1));
-
 	if (s == NULL)
 	{
 		return (NULL);
 	}
 	
-	int l;
-
 	for (l = 0; l < i + j + 1; l++)
 	{
-		s[l] = s1[l] + s2[l]
+		s[l] = s1[l] + s2[l];
 	}
 	s[l] = '\0';
 
 	return (s);
 }
-
-
