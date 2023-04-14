5188c41b337b1ff5d167df8f14765b684d504162
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-13 17:37:58 -0700

    check

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,10 @@
 
 /**
  * program to check the malloc function
- * malloc_checked()-function to check malloc
- *@b : the number of bytes to allocate
- *Return : 98 if faillure, pointer if success
+ * malloc_checked - function to check malloc
+ *@b: the number of bytes to allocate
+
+ *Return: 98 if faillure, pointer if success
  */
 
 void *malloc_checked(unsigned int b)
