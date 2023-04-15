61ddf4662bf4e60ff762bace33170637822e82e3
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-14 18:39:19 -0700

    add 0malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,16 @@
 #include<stdlib.h>
 
 /**
- * program to check the malloc function
+ * description:program to check the malloc function
  * malloc_checked - function to check malloc
  *@b: the number of bytes to allocate
-
  *Return: 98 if faillure, pointer if success
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
+	
 	if (p == NULL)
 	{
 		exit(98);
