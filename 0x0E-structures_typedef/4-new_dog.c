5f588da35dfe814475fe9dd4f7a1d43a6880e79d
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-18 07:34:35 -0700

    add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,28 +3,31 @@
 #include"dog.h"
 
 /**
- * progrzm copy
- * new_dog - function for new struct
+ * 
+ * *new_dog - function for new struct.
+ *
  * @name : name
  * @age : age
  * @owner: owner
+ *
  * Return: new structure
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(dog_t));
+	
 	if (new_dog == NULL)
-	{
-		return (NULL);
-	}
-	new_dog->name = strdup(name);
-	if (new_dog->name == NULL) 
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL)
 	{
