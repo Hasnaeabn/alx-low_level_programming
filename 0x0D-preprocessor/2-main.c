886372cc429922c17d1a9af15455285a27c71725
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-17 18:33:14 -0700

    coomt

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * programm to show the file name
+ * main - prints the name of the file
+ * 
+ * Return: void 
+ */
+
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
