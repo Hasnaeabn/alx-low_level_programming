a8b038a566608432f1f442821de5ffc73f9a2b58
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-18 07:01:16 -0700

    add dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,12 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * dog : new struct for dog names age and owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
