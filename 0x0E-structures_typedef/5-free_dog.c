212f8bc5e08f7e417f5dd503d9dd4d39fab294d4
Hasnaeabn <hasna.abounouh@gmail.com>
2023-04-18 07:37:34 -0700

    add

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,19 +1,20 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * program
- * free_dog - function to free dog
- * @d: structure to free
- * Return: void
+ * free_dog - function to free dog.
+ *
+ * @d: structure to free.
+ *
+ * Return: void.
  */
 
 
-void free_dog(dog_t *d) 
+void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-	free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
