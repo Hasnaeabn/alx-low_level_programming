e4abe9ecb77d8e1ed9bb651c9fa033b3ff88ea3a
Hasnaeabn <125559271+Hasnaeabn@users.noreply.github.com>
2023-07-21 16:20:07 +0200

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,15 +7,20 @@
 
 
 
-int sum_them_all(const unsigned int n, ...)
-{
-	unsigned int i;
-	int sum = 0;
-	const unsigned int* ptr = &n + 1;
-	
-	for (i = 0; i < n; i++)
-	{
-		sum += *(ptr++);
-	}
-	return sum;
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...) {
+  int sum = 0;
+  va_list args;
+  va_start(args, n);
+
+  for (unsigned int i = 0; i < n; i++) {
+    sum += va_arg(args, int);
+  }
+
+  va_end(args);
+
+  return sum;
 }
+
